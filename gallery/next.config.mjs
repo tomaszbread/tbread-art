672aad2d0a857e45d862875ebffd5af7287c960a
Tomasz Bread <tomaszbread@gmail.com>
2024-10-14 14:39:59 +0200

    next

--- gallery/next.config.mjs
+++ gallery/next.config.mjs
@@ -4,9 +4,6 @@ const nextConfig = {
     deviceSizes: [320, 420, 768, 1024, 1200],
     loader: "default",
     domains: ["res.cloudinary.com"],
-  },
-  serverRuntimeConfig: {
-    port: process.env.PORT || 3000,
   }
 };
 
